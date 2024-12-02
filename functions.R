16870e75d6d594c3ee35353c13cfa45c7131b786
simon-mellergaard <simonmellergaard@gmail.com>
2024-12-02 08:58:32 +0000

    A file containing basic functions

---
+++ functions.R
@@ -0,0 +1,11 @@
+# This function will add two numbers together
+func1 <- function(a, b) {
+  return(a + b)
+}
+
+# This function will multiply two numbers
+func2 <- function(a, b) {
+  return(a * b)
+}
+
+# These are some great functions
